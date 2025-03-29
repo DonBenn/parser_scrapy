3010420f7965e84af5fbfb7117eaeec75ecc9654
Denis Bessonov <denibessono@yandex.ru>
2025-03-29 18:25:29 +0300

    BASE_DIR test fix

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,6 +1,10 @@
 import csv
 import os
 from datetime import datetime
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class PepParsePipeline:
@@ -22,7 +26,7 @@ class PepParsePipeline:
     def close_spider(self, spider):
         total = sum(self.status_count.values())
         formatted_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        filename = f'results/status_summary_{formatted_time}.csv'
+        filename = BASE_DIR / f'results/status_summary_{formatted_time}.csv'
 
         os.makedirs('results', exist_ok=True)
 
